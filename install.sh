3e951412c22196330aa83ad10db37726ce64d7c4
headless chicken wing <161917744+headless-chicken-wing@users.noreply.github.com>
2026-03-05 18:03:32 -0600

    install.sh update

--- install.sh
+++ install.sh
@@ -123,8 +123,8 @@ os_check() {
   fi
 
   if [[ "${VERSION_CODENAME}" != "trixie" ]]; then
-    msg_error "This script is intended for Debian Trixie. Detected: ${PRETTY_NAME}. Exiting."; elif
-	[[ "${ID_LIKE}" != "*debian*" ]]; then
+    msg_error "This script is intended for Debian Trixie. Detected: ${PRETTY_NAME}. Exiting."
+   elif	[[ "${ID_LIKE}" != "*debian*" ]]; then
     msg_error "This script is intended for Debian systems. Detected: ${PRETTY_NAME}. Exiting."
     exit 1
   fi
