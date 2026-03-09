3a87b93f554a1796350e13ff27612a8f99e8dcde
headless chicken wing <161917744+headless-chicken-wing@users.noreply.github.com>
2026-03-09 05:42:31 -0500

    replacing with updates

--- bin/hello.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "What's your fucking name asshole?"
-
-read name
-
-echo "Go fuck yourself, $name"
--- bin/roku-press-forward.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from roku import Roku
-roku = Roku('192.168.1.35')
-
-
-roku.right()
-
-quit()
